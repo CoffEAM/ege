a6c89043a7bff484ecee89a2aae7194ef0793ba8
AMamedov <aziz050907@gmail.com>
2024-12-24 18:48:41 +0200

    С функцией переода в 3ную сс

--- егкр-21-12-24/Task-5-19237.py
+++ егкр-21-12-24/Task-5-19237.py
@@ -1,5 +1,23 @@
-print('hello')
-print('boriz')
+def convert(num):
+    res = ''
+    while num:
+        res += str(num%3)
+        num //= 3
+    return res[::-1]
+
+
+ans = []
+for n in range(1, 10000):
+    r = convert(n)
+    if n%3==0:
+        r = r + r[-2:]
+    else:
+        r = r + convert(sum(map(int, r)))
+    r = int(r, 3)
+    if r%2==0 and r>220:
+        ans.append(r)
+print(min(ans))
+
 
 
 
