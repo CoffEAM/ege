820a31391c10468a4cd9a50600e638cf0f818110
aziz0 <aziz050907@gmail.com>
2025-01-07 16:41:46 +0200

    Задание-8 ФИПИ2025 Вар.1 Неправильное

--- другое/ФИПИ2025 Вариант 1/Task-8.py
+++ другое/ФИПИ2025 Вариант 1/Task-8.py
@@ -1,4 +1,4 @@
-from itertools import product
+from itertools import product, permutations
 
 alph = '012345'
 
@@ -6,7 +6,13 @@ cnt = 0
 for i in product(alph, repeat=6):
     i = ''.join(i)
     if i[0]!='0' and i.count('0')==1:
-        if '202' not in i and '204' not in i and '402' not in i and '404' not in i:
+        flag = False
+        for val in permutations('0135', 3):
+            val = ''.join(val)
+            if val[1] == '0':
+                if val not in i:
+                    flag = True
+        if flag:
             cnt += 1
 print(cnt)
 
