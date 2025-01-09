131b32ede10057e7d67fc5a12737db2f3bc7786a
Student <aziz050907@gmail.com>
2025-01-09 12:37:05 +0200

    Задание-24 Пробник

---
+++ другое/Пробник 09.01.25/Task-24.py
@@ -0,0 +1,15 @@
+from string import ascii_uppercase
+alph = ascii_uppercase
+with open('txt/24_11636.txt') as file:
+    st = file.readline()
+
+for i in alph:
+    if i != 'A':
+        st = st.replace(i, '*')
+st = st.replace('A', 'A A')
+st = st.replace('AA', ' ')
+st = st.split()
+print(max(len(i) for i in st if i.count('A')==2))
+
+
+
