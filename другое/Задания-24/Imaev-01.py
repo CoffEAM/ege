456a690838794cc158403a7cbf2e619e57343e2d
aziz0 <aziz050907@gmail.com>
2025-01-11 17:23:55 +0200

    Легкое задание. Имаев 6 часовой вэб 2024

--- другое/Задания-24/Imaev-01.py
+++ другое/Задания-24/Imaev-01.py
@@ -1,7 +1,12 @@
 with open('imaev/24_1.txt') as file:
     st = file.readline()
 
-st = st.replace('D', ' ').replace('E', ' ')
+st = st.replace('AA', 'A A').replace('BB', 'B B')
+st = st.replace('CC', 'C C').replace('DD', 'D D')
+st = st.replace('EE', 'E E')
+st = st.replace('AA', 'A A').replace('BB', 'B B')
+st = st.replace('CC', 'C C').replace('DD', 'D D')
+st = st.replace('EE', 'E E')
 
 print(len(max(st.split(), key=len)))
 
