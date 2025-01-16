ef2305e44b66b575b696a1b93a3f9a3968f3ea6f
aziz0 <aziz050907@gmail.com>
2025-01-16 22:42:55 +0200

    ДЗ на 17.01.25 - Задание-8 №11292 НЕПРАВИЛЬНОЕ Необычное
    https://kompege.ru/variant?kim=25075893

--- ДЗ-17-01-25/Task-8-11292.py
+++ ДЗ-17-01-25/Task-8-11292.py
@@ -7,8 +7,11 @@ count = 0
 for val in product(alph[:16], repeat=5):
     val = ''.join(val)
     if val.count('6') == 2:
+        flag = False
         for i in product('2468ACE', repeat=3):
             i = ''.join(i)
             if i[1] == '6' and i not in val:
-                count += 1
+                flag = True
+        if flag:
+            count += 1
 print(count)
