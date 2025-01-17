f10d97551fd827262256f9ee465ef17111d55956
AMamedov <aziz050907@gmail.com>
2025-01-17 18:55:19 +0200

    Задание-1 №14443

---
+++ 17-01-25/Task-1-14443.py
@@ -0,0 +1,10 @@
+from itertools import permutations
+
+graph = 'DB DA DC DE DF AB BC AE CF GF EG'.split()
+matrix = '23567 145 146 23 127 137 156'.split()
+print(*range(1, 8))
+for i in permutations('ABCDEFG'):
+    if all(str(i.index(x) + 1) in matrix[i.index(y)] for x, y in graph):
+        print(*i)
+
+
