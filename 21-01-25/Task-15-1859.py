86c368b2fb90ffd97b98e5a5392a8bd84035ccbf
AMamedov <aziz050907@gmail.com>
2025-01-21 19:41:11 +0200

    Задание-15 №1859 https://kompege.ru/variant?kim=25077099

---
+++ 21-01-25/Task-15-1859.py
@@ -0,0 +1,13 @@
+def f(A):
+    for x in range(1000):
+        for y in range(1000):
+            f = (2*x + y != 70) or (x < y) or (A < x)
+            if not f:
+                return False
+    return True
+
+for A in range(1000):
+    if f(A):
+        print(A)
+
+
