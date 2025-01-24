e2c49c66aa3593bc98dd7d026a093dcc80a4e5f9
AMamedov <aziz050907@gmail.com>
2025-01-24 18:29:54 +0200

    commmmetet

--- 24-01-25/Task-8-17798.py
+++ 24-01-25/Task-8-17798.py
@@ -8,7 +8,7 @@ for pos, val in enumerate(product(sorted('МИНУС'), repeat=4), start=1):
     val = ''.join(val)
     sogls = sum(1 for i in val if i in sogl)
     gls = sum(1 for i in val if i in gl)
-    if sogls < gls:
+    if sogls >= gls:
         ans.append(pos)
 print(ans[-1])
 
