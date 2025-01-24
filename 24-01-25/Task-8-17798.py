14c90b1db27284e6b1230619e8a32495fdfd4447
AMamedov <aziz050907@gmail.com>
2025-01-24 18:11:24 +0200

    Задание-8 №10657 https://kompege.ru/variant?kim=25077879

---
+++ 24-01-25/Task-8-17798.py
@@ -0,0 +1,15 @@
+from itertools import product, repeat
+
+sogl = 'МНС'
+gl = 'ИУ'
+
+ans = []
+for pos, val in enumerate(product(sorted('МИНУС'), repeat=4), start=1):
+    val = ''.join(val)
+    sogls = sum(1 for i in val if i in sogl)
+    gls = sum(1 for i in val if i in gl)
+    if sogls < gls:
+        ans.append(pos)
+print(ans[-1])
+
+
