8e3717904f6ecebf1b21a8c5d010746352350c55
AMamedov <aziz050907@gmail.com>
2025-01-28 18:06:44 +0200

    commit

--- ДЗ/ДЗ-24-01-25/Task-14.py
+++ ДЗ/ДЗ-24-01-25/Task-14.py
@@ -5,7 +5,7 @@ alph = digits + ascii_uppercase
 for x in range(1, 150):
     num1 = 5*150**4 + 1*150**3 + x*150**2 + 2*150 + 9
     #num2 = #
-    num = num1 + num2
+    num = num1# + #num2
     if num%149==0:
         print(num//149)
 
