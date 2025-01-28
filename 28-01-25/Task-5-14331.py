c0fb3f68c492e473c3264151ad914d310e9e846b
AMamedov <aziz050907@gmail.com>
2025-01-28 18:49:49 +0200

    Задание-5 №14331 https://kompege.ru/variant?kim=25078582

---
+++ 28-01-25/Task-5-14331.py
@@ -0,0 +1,19 @@
+def con(num):
+    res = ''
+    while num:
+        res += str(num%3)
+        num //= 3
+    return res[::-1]
+ans = []
+for n in range(1, 10000):
+    r = con(n)
+    if sum(map(int, r))%3==0:
+        r += '212'
+    else:
+        r += con((sum(map(int, r)) % 3) * 2)
+    r = int(r, 3)
+    if r >490:
+        ans.append(r)
+print(min(ans))
+
+
