26cbc034053ce161e6401a1abc844ddf2b18de96
aziz0 <aziz050907@gmail.com>
2025-01-30 22:59:19 +0200

    Задание-15 №19247 ДЗ-31-01-25 https://kompege.ru/variant?kim=25078940

---
+++ ДЗ/ДЗ-31-01-25/Task-15-19247.py
@@ -0,0 +1,15 @@
+def f(a):
+    for x in range(1, 1000):
+        for y in range(1, 1000):
+            f = (x - 3*y < a) or (y > 400) or (x > 56)
+            if not f:
+                return 0
+    return 1
+
+for a in range(1000):
+    if f(a):
+        print(a)
+        break
+
+
+
