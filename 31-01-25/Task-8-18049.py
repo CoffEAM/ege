c745610a244b91accccdd2240a3a1758b8ff6fac
AMamedov <aziz050907@gmail.com>
2025-01-31 20:00:16 +0200

    commit

--- 31-01-25/Task-8-18049.py
+++ 31-01-25/Task-8-18049.py
@@ -6,11 +6,11 @@ cnt = 0
 for num in product('012345678', repeat=7):
     num = ''.join(num)
     if num[0] not in '0246':
-        flag = False
+        flag = 0
         for k in arr:
-            if k != num[-3:]:
-                flag = True
-        if flag:
+            if k == num[-3:]:
+                flag += 1
+        if flag==0:
             cnt += 1
 print(cnt)
 
