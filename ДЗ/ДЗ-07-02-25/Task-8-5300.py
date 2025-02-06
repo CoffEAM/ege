edd60f4f61fbbf95b09b7a9173effad8c637fd14
aziz0 <aziz050907@gmail.com>
2025-02-06 22:14:39 +0200

    Задание-8 №5300 ДЗ-07-02-25 https://kompege.ru/variant?kim=25080370

--- ДЗ/ДЗ-07-02-25/Task-8-5300.py
+++ ДЗ/ДЗ-07-02-25/Task-8-5300.py
@@ -3,13 +3,12 @@ from itertools import permutations
 answer = 0
 for sentence in set(permutations('ХОЧУ В ВУЗ')):
     sentence = ''.join(sentence)
-    if len(sentence.split())>=2:
-        cnt = 0
-        for word in sentence.split():
-            if word[0]=='У':
-                cnt += 1
+    if len(sentence.split())==3:
+        cnt = sum(1 for word in sentence.split() if word[0]=='У')
         if cnt == 0:
             answer += 1
+    if sentence=='ХОЧУ В ВУЗ':
+        print('god damn it не хочу в вуз')
 print(answer)
 
 
