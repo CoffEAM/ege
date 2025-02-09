eb3fa1595d38f805f214ef2399c5a2f38642cad2
aziz0 <aziz050907@gmail.com>
2025-02-09 12:37:54 +0200

    Задания-25 ФИПИ2025 Вариант-3

---
+++ другое/ФИПИ2025 Вариант 3/Task-25.py
@@ -0,0 +1,14 @@
+for num in range(800001, 9000001):
+    r = 0
+    for delitel in range(2, num//2):
+        if num%delitel==0:
+            r += delitel
+    if str(r)[-1]=='3':
+        print(num, r)
+
+#800004 682713
+#800024 300023
+#800064 1157023
+#800068 200023
+#800084 211073
+
