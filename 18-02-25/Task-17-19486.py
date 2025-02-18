f98ccc3e3662e9611aa0836b2bd5c2318e348f67
AMamedov <aziz050907@gmail.com>
2025-02-18 18:48:47 +0200

    commit

--- 18-02-25/Task-17-19486.py
+++ 18-02-25/Task-17-19486.py
@@ -4,7 +4,7 @@ with open('txt/17_19486.txt') as file:
 cnt = sum(1 for i in arr if str(i)[-1]=='7')
 
 ans = []
-for i in range(len(arr)-2):
+for i in range(len(arr)-1):
     para = arr[i:i+2]
     u1 = sum([1 for i in para if i<0])
     u2 = sum([1 for i in para if i>0])
