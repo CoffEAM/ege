6552ea616cd3837e9926ff7e7cc2b5c8af67ead1
AMamedov <aziz050907@gmail.com>
2025-02-18 19:47:47 +0200

    Задание-25 №14440 мега способ нахождения произведения https://kompege.ru/variant?kim=25083166

--- 18-02-25/Task-25-14440.py
+++ 18-02-25/Task-25-14440.py
@@ -1,14 +1,14 @@
 from fnmatch import fnmatch
 
+
 def is_prime(num):
-    for i in range(2, int(num**0.5) + 1):
-        if num%i ==0:
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
             return False
     return True
 
-for i in range(315670, 10**7):
+
+for i in range(315670, 10 ** 7):
     if fnmatch(str(i), '31*567?') and is_prime(i):
-        p = 1
-        for k in str(i):
-            p *= int(k)
+        p = eval('*'.join(list(str(i))))
         print(i, p)
