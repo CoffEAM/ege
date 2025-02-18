1bc733fb59f18360587cceb368115ac75dbc70af
AMamedov <aziz050907@gmail.com>
2025-02-18 18:23:11 +0200

    Задание-17 №19486 https://kompege.ru/variant?kim=25083158

---
+++ 18-02-25/Task-17-19486.py
@@ -0,0 +1,13 @@
+with open('txt/17_19486.txt') as file:
+    arr = [int(i) for i in file]
+
+cnt = sum(1 for i in arr if str(i)[-1]=='7')
+
+ans = []
+for i in range(len(arr)-2):
+    para = arr[i:i+2]
+    u1 = sum([1 for i in para if i<0])
+    u2 = sum([1 for i in para if i>0])
+    if u1==u2==1 and sum(para)<cnt:
+        ans.append(sum(para))
+print(len(ans), max(ans))
