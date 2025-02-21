09f4e56ac5d5a401086f9952d525c61431e79c95
AMamedov <aziz050907@gmail.com>
2025-02-21 19:12:53 +0200

    Задание-25 №18591 https://kompege.ru/variant?kim=25083844

--- 21-02-25/Task-25-11955.py
+++ 21-02-25/Task-25-11955.py
@@ -2,5 +2,5 @@ from fnmatch import fnmatch
 
 for i in range(1021574 - 1021574%133, 10**10, 133):
     if str(i)[1] in '02468':
-        if fnmatch(str(i), '1?2157*4'):
+        if fnmatch(str(i), '1[02468]2157*4'):
 
