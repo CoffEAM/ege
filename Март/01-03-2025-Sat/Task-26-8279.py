8e8b52df6167d04152b1a24000fee0d1ad3e58b3
AMamedov <aziz050907@gmail.com>
2025-03-01 19:49:50 +0200

    Задание-26 №8279 Суббота

--- Март/01-03-2025-Sat/Task-26-8279.py
+++ Март/01-03-2025-Sat/Task-26-8279.py
@@ -2,6 +2,16 @@ with open(r'txt\26_8279.txt') as file:
     t, N, M = map(int, file.readline().split())
     data = [list(map(int, i.split())) for i in file]
 
-clients = sorted(data, key=lambda x: (-x[0], x[1], -x[2]), reverse=True)
-clients = [i for i in clients if i[2] <= M]
+clients = [i for i in data if i[2] <= M]
+clients = sorted(clients, key=lambda x: (-x[0], x[1], -x[2]), reverse=True)
+
 print(clients)
+buy_items = []
+for i in clients:
+    summ = sum(1 for k in range(i[0] + i[0]%6, i[1]+1, 6))
+    time = sum(1 for j in range(i[0], i[1]+1))
+    buy_items.append([time , summ])
+
+print(*max(buy_items))
+
+
