38e2c5d41ef5b3255eefd9ea27cef1a983ab39d6
aziz0 <aziz050907@gmail.com>
2025-03-02 13:05:37 +0200

    Задание-13 №18955 КИМ №25071427 https://kompege.ru/variant?kim=25071427

---
+++ другое/КИМ №25071427 Вариант 7/Task-13-18955.py
@@ -0,0 +1,8 @@
+from ipaddress import ip_network
+
+# for i in range(16, 33):
+#     for ip in ['200.154.190.12', '200.154.184.0']:
+#         net = ip_network(f'{ip}/{i}', False)
+#         print(i)
+
+print(bin(190&184))
