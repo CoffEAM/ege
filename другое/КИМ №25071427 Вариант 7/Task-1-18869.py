bd267d2a89feaf3c7bb7f6471af6d26bef7df928
aziz0 <aziz050907@gmail.com>
2025-03-02 12:01:06 +0200

    Задание-1 №18869 КИМ №25071427 https://kompege.ru/variant?kim=25071427

---
+++ другое/КИМ №25071427 Вариант 7/Task-1-18869.py
@@ -0,0 +1,8 @@
+from itertools import permutations
+
+graph = 'DE DC EF CF EH CB FG HG BA GA HA'.split()
+matrix = '568 36 247 368 178 124 35 145'.split()
+print(*range(1, 9))
+for i in permutations('ABCDEFGH'):
+    if all(str(i.index(x) + 1) in matrix[i.index(y)] for x, y in graph):
+        print(*i)
