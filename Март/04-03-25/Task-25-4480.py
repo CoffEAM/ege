f45f595fa73f6764107a3a2ff2880da6a7235f39
aziz0 <aziz050907@gmail.com>
2025-03-04 18:47:03 +0200

    commit

--- Март/04-03-25/Task-25-4480.py
+++ Март/04-03-25/Task-25-4480.py
@@ -1,6 +1,6 @@
 def f(num):
     res = set()
-    for i in range(1, int(num**0.5)+1):
+    for i in range(2, int(num**0.5)+1):
         if num%i==0:
             res |= {i, num//i}
     res = sorted(res)
