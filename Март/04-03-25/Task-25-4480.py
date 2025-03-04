96f0b271af476298fb6f0087eaa1595e4a5aa189
aziz0 <aziz050907@gmail.com>
2025-03-04 18:32:45 +0200

    Задание-25 №4480 https://kompege.ru/variant?kim=25086029

---
+++ Март/04-03-25/Task-25-4480.py
@@ -0,0 +1,23 @@
+def f(num):
+    res = set()
+    for i in range(1, int(num**0.5)+1):
+        if num%i==0:
+            res |= {i, num//i}
+    res = sorted(res)
+    summ = sum(res)
+    comb = 1
+    for i in res:
+        comb *= i
+    if summ%2==1 and comb%2==1:
+        if len(res)>10:
+            return len(res)
+    return 0
+
+cnt = 0
+for i in range(800001, 10**10):
+    u = f(i)
+    if u:
+        print(i, u)
+        cnt += 1
+        if cnt == 6:
+            break
