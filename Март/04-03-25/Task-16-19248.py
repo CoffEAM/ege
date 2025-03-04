f4997fe0fc4a728caf92eebf728de0ae9e77514d
aziz0 <aziz050907@gmail.com>
2025-03-04 19:13:12 +0200

    Задание-16 №19248 https://kompege.ru/variant?kim=25086035

---
+++ Март/04-03-25/Task-16-19248.py
@@ -0,0 +1,9 @@
+from sys import setrecursionlimit
+setrecursionlimit(5000)
+def f(n):
+    if n < 5:
+        return n
+    else:
+        return 2 * n * f(n - 4)
+
+print((f(13766) - 9*f(13762))/f(13758))
