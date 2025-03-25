b3cee66fd99bcf2c18f619cb5064d694552151fd
AMamedov <aziz050907@gmail.com>
2025-03-25 18:16:16 +0200

    Задание-12 №11231 https://kompege.ru/variant?kim=25090443

---
+++ Март/25-03-25/Task-12-11231.py
@@ -0,0 +1,11 @@
+ans = 2**20
+for n in range(4, 2000):
+    st = n * '3' + '5'
+    while '23' in st or '5333' in st or '33333' in st:
+        st = st.replace('23', '3', 1)
+        st = st.replace('5333', '32', 1)
+        st = st.replace('33333', '55', 1)
+    summ = sum(map(int, st))
+    if summ < ans:
+        ans = summ
+print(ans)
