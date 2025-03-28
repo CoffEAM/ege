11b25c26d48a70fdb5652ed804b23d9ca7f95afb
AMamedov <aziz050907@gmail.com>
2025-03-28 19:30:25 +0200

    Задание-19-21 №4624 https://kompege.ru/variant?kim=25091042

---
+++ Март/28-03-25/Task-19-21-4624.py
@@ -0,0 +1,9 @@
+def f(x, s):
+    if x >= 165: return s % 2 == 0
+    if s == 0: return False
+    h = [f(x + 1, s - 1), f(x * 2, s - 1)]
+    return any(h) if (s-1) % 2 == 0 else all(h)
+
+print(*[s for s in range(1, 165) if f(s, 2)])
+print(*[s for s in range(1, 165) if f(s, 3) and not f(s, 1)])
+print(*[s for s in range(1, 165) if f(s, 4) and not f(s, 2)])
