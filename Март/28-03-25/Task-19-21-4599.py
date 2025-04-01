6ab731135f68225255044be7c71791dec480e7de
AMamedov <aziz050907@gmail.com>
2025-04-01 18:21:16 +0200

    Задание-19-21 №20907 Апробация 05.03.25

--- Март/28-03-25/Task-19-21-4599.py
+++ Март/28-03-25/Task-19-21-4599.py
@@ -1,8 +1,9 @@
 def f(x, y, s):
     if x + y >= 259: return s % 2 == 0
     if s == 0: return False
-    h = [f(x +1, y, s - 1), f(x, y + 1, s - 1), f(x * 2, y, s - 1), f(x, y * 2, s - 1)]
+    h = [f(x + 1, y, s - 1), f(x, y + 1, s - 1), f(x * 2, y, s - 1), f(x, y * 2, s - 1)]
     return any(h) if (s - 1) % 2 == 0 else all(h)
+
 
 print(*[s for s in range(1, 242) if f(17, s, 2)])
 print(*[s for s in range(1, 242) if f(17, s, 3) and not f(17, s, 1)])
