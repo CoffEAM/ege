ec7292a68fc203c8cf237e6f345f75b2998b4180
AMamedov <aziz050907@gmail.com>
2025-04-11 19:07:35 +0200

    Задание-13 №11794 https://kompege.ru/variant?kim=25093689

--- Апрель/11-04-25/Task-13-11794.py
+++ Апрель/11-04-25/Task-13-11794.py
@@ -1,12 +1,24 @@
+# from ipaddress import ip_network
+#
+# for a in range(256):
+#     net = ip_network(f'223.167.{a}.167/255.255.255.192', False)
+#     ans = True
+#     for ip in net:
+#         lcnt = f'{int(ip):032b}'[:16].count('0')
+#         rcnt = f'{int(ip):032b}'[16:].count('0')
+#         if lcnt > rcnt:
+#             ans = False
+#     if ans:
+#         print(a)
+
 from ipaddress import ip_network
 
 for a in range(256):
     net = ip_network(f'223.167.{a}.167/255.255.255.192', False)
-    ans = True
     for ip in net:
         lcnt = f'{int(ip):032b}'[:16].count('0')
         rcnt = f'{int(ip):032b}'[16:].count('0')
-        if lcnt > rcnt:
-            ans = False
-    if ans:
+        if not (lcnt <= rcnt):
+            break
+    else:
         print(a)
