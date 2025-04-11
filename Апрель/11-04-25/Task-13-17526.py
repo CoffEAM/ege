50e9dd99051407e232d70916098aae5b97eb4387
AMamedov <aziz050907@gmail.com>
2025-04-11 18:46:55 +0200

    Задание-13 №17526 https://kompege.ru/variant?kim=25093689

--- Апрель/11-04-25/Task-13-17526.py
+++ Апрель/11-04-25/Task-13-17526.py
@@ -3,7 +3,7 @@ from ipaddress import ip_network
 net = ip_network('172.16.128.0/255.255.192.0', False)
 ans = 0
 for ip in net:
-    ip = bin(int(ip))[2:]
+    ip = f'{int(ip):032b}'
     if ip.count('1') % 2 == 1:
         ans += 1
 print(ans)
