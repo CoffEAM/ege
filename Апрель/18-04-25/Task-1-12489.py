a80cfc2798b1c42eeb8fa15eda2e81de5d03e762
AMamedov <aziz050907@gmail.com>
2025-04-18 18:09:39 +0200

    Задание-1 №12489 https://kompege.ru/variant?kim=25093686

---
+++ Апрель/18-04-25/Task-1-12489.py
@@ -0,0 +1,8 @@
+from itertools import permutations
+
+graph = 'АБ АВ АГ БГ ВГ ГЕ ГЗ ГЖ ГД ЕЗ ДЖ ЗЖ'.split()
+matrix = '235 13 1245678 36 13 347 368 37'.split()
+print(*range(1, 9))
+for i in permutations('АБВГДЕЖЗ'):
+    if all(str(i.index(x) + 1) in matrix[i.index(y)] for x, y in graph):
+        print(*i)
