6b7046460428c6352b4dbb7b883e2a7c412f7068
AMamedov <aziz050907@gmail.com>
2025-05-13 18:55:00 +0200

    Задание-24 №1866 https://kompege.ru/variant?kim=25099953

--- 24е задания по годам/Task-24-1975.py
+++ 24е задания по годам/Task-24-1975.py
@@ -1,6 +1,7 @@
 with open('txt/24_1975.txt') as file:
     st = file.readline()
 
-st = st.replace('PP', 'P P')
+while 'PP' in st:
+    st = st.replace('PP', 'P P')
 st = st.split()
 print(len(max(st, key=len)))
