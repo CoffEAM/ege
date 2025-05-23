3058cd0b743a5177a5701091c1347bd23ef9aadc
AMamedov <aziz050907@gmail.com>
2025-05-23 19:06:17 +0200

    Задание-24 №21421 https://kompege.ru/variant?kim=25099958

---
+++ 24е задания по годам/Task-24-21421.py
@@ -0,0 +1,8 @@
+from re import finditer
+
+with open('txt/24_21421.txt') as file:
+    st = file.readline()
+#01234567891011
+pattern = r'[1-9AB]([0-9AB])+[02468A]'
+res = [i.group() for i in finditer(pattern, st)]
+print(len(max(res, key=len)))
