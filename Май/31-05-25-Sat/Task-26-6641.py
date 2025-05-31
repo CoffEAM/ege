2358c30d022cbb94eee863204cd3950d379eb33f
aziz0 <aziz050907@gmail.com>
2025-05-31 16:43:21 +0200

    Задание-26 №6641 https://kompege.ru/variant?kim=25101707

--- Май/31-05-25-Sat/Task-26-6641.py
+++ Май/31-05-25-Sat/Task-26-6641.py
@@ -4,4 +4,19 @@ with open('txt/26_6641.txt') as file:
 
 data = [[int(i[0]), i[1]] for i in data]
 data = sorted(data)
-print(data)
+s = []
+w = []
+for i in data.copy():
+    if i[0] <= M:
+        M -= i[0]
+        if i[1] == 'S': s.append(i[0])
+        else: w.append(i[0])
+        data.remove(i)
+
+for i in data:
+    if i[1] == 'S' and M + w[-1] - i[0] >= 0:
+        M += w[-1] - i[0]
+        s.append(i[0])
+        w.pop()
+print(len(s), M)
+
