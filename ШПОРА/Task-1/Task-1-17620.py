b4b53e653c77e19c6138cd4ae5991e92cacf4ee7
aziz0 <aziz050907@gmail.com>
2025-06-02 18:35:00 +0200

    Шпора по №1 ДЗ на 03-06 Задание №17620 https://kompege.ru/variant?kim=25102451

--- ШПОРА/Task-1/Task-1-17620.py
+++ ШПОРА/Task-1/Task-1-17620.py
@@ -1,8 +1,7 @@
 from itertools import permutations
 
-graph = 'AB AF FB FE BD ED EC DG CG'.split()
+graph = 'AB BD DG GC CE EF FA FB ED'.split()
 matrix = '256 134 267 27 16 135 34'.split()
-
 print(*range(1, 8))
 for i in permutations('ABCDEFG'):
     if all(str(i.index(x) + 1) in matrix[i.index(y)] for x, y in graph):
