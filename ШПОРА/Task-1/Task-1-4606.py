a0142d711a159d8615501ae3a70042d4b52c8f2a
aziz0 <aziz050907@gmail.com>
2025-06-02 18:18:13 +0200

    Шпора по №1 ДЗ на 03-06 Задание №4606 https://kompege.ru/variant?kim=25102451

--- ШПОРА/Task-1/Task-1-4606.py
+++ ШПОРА/Task-1/Task-1-4606.py
@@ -1,9 +1,8 @@
 from itertools import permutations
 
-graph = 'AC AB AF BF CE CD FG DH HE EG'.split()
+graph = 'af fg ge ec ca cd dh he ab bf'.split()
 matrix = '68 47 45 237 368 15 248 157'.split()
-
 print(*range(1, 9))
-for i in permutations('ABCDEFGH'):
+for i in permutations('abcdefgh'):
     if all(str(i.index(x) + 1) in matrix[i.index(y)] for x, y in graph):
         print(*i)
