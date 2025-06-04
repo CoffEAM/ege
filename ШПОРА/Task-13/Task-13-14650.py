79b72dae8e32ff4a417f96c49c1f52c61cb5823c
Student-14 <aziz050907@gmail.com>
2025-06-04 15:07:36 +0200

    Шпора по №13 Задание №14650 https://kompege.ru/variant?kim=25102972

---
+++ ШПОРА/Task-13/Task-13-14650.py
@@ -0,0 +1,11 @@
+from ipaddress import ip_address, ip_network
+
+ip1 = ip_address('216.54.187.235')
+ip2 = ip_address('216.54.174.128')
+for mask in range(10, 31)[::-1]:
+    net1 = ip_network(f'{ip1}/{mask}', False)
+    net2 = ip_network(f'{ip2}/{mask}', False)
+    if net1 != net2:
+        if ip1 in net1.hosts() and ip2 in net2.hosts():
+            print(mask)
+            break
