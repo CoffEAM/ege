51544818cc5bdbbedf0e2b05900671ef985382b1
Student-14 <aziz050907@gmail.com>
2025-06-04 15:43:02 +0200

    Шпора по №14 Задание №9838 https://kompege.ru/variant?kim=25102972

---
+++ ШПОРА/Task-15/Task-15-9838.py
@@ -0,0 +1,12 @@
+def f(a):
+    for x in range(1000):
+        for y in range(1000):
+            u = (x + 2 * y > a) or (y < x) or (x < 30)
+            if not u:
+                return False
+    return True
+
+for a in range(1000)[::-1]:
+    if f(a):
+        print(a)
+        break
