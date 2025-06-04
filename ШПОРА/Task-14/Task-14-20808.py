b12259819b424d8d26a1ae6aa657aff2a125de05
Student-14 <aziz050907@gmail.com>
2025-06-04 16:08:49 +0200

    Шпора по №14 Задание №20808 https://kompege.ru/variant?kim=25102972

---
+++ ШПОРА/Task-14/Task-14-20808.py
@@ -0,0 +1,9 @@
+maxx = 73
+for x in range(1, 2031):
+    num = 7 ** 170 + 7 ** 100 - x
+    res = ''
+    while num:
+        res += str(num % 7)
+        num //= 7
+    if res.count('0') == maxx:
+        print(x)
